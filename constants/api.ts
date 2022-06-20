27fe69ae180e72d7c2715dea20c00c159a948fde
Alik Ovesyan <icoleksander.Ovesian@americanwell.com>
2022-06-20 20:20:01 +0300

    spotify auth

---
+++ constants/api.ts
@@ -0,0 +1,13 @@
+export const API_CONFIG = {
+	clientId: '34b64bd7b7e84269903e2321afe266a5',
+	cleintSecret: '4a4917b08e8d4add94c7cde3b6204784',
+};
+
+// alikClientId: '34b64bd7b7e84269903e2321afe266a5'
+// alikClientSecret: '4a4917b08e8d4add94c7cde3b6204784'
+
+//  alexClientId: '',
+// 	alexClientSecret: '',
+
+// 	andrewClientId: '',
+// 	andrewClientSecret: '',
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,41 +1,81 @@
-import { Button, StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View, StyleSheet, KeyboardAvoidingView, Button } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ResponseType, useAuthRequest } from 'expo-auth-session';
+import axios from 'axios';
+import { API_CONFIG } from '../constants/api';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+export default function TabOneScreen() {
+	const [token, setToken] = useState('');
+	const [request, response, promptAsync] = useAuthRequest(
+		{
+			responseType: ResponseType.Token,
+			clientId: API_CONFIG.clientId,
+			scopes: [
+				'user-read-currently-playing',
+				'user-read-recently-played',
+				'user-read-playback-state',
+				'user-top-read',
+				'user-modify-playback-state',
+				'streaming',
+				'user-read-email',
+				'user-read-private',
+			],
+			usePKCE: false,
+			redirectUri: 'exp://192.168.100.14:19000',
+		},
+		{
+			authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+			tokenEndpoint: 'https://accounts.spotify.com/api/token',
+		}
+	);
 
-export default function TabOneScreen({
-  navigation,
-}: RootTabScreenProps<'TabOne'>) {
-  const onLogin = () => {};
+	useEffect(() => {
+		if (response?.type === 'success') {
+			const { access_token } = response.params;
+			setToken(access_token);
+		}
+	}, [response]);
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor='#eee'
-        darkColor='rgba(255,255,255,0.1)'
-      />
-      <EditScreenInfo path='/screens/TabOneScreen.tsx' />
-      <Button title='Login' onPress={onLogin} />
-    </View>
-  );
+	useEffect(() => {
+		if (token) {
+			axios('https://api.spotify.com/v1/me', {
+				method: 'GET',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+					Authorization: 'Bearer ' + token,
+				},
+			})
+				.then(response => {
+					console.log(response.data);
+				})
+				.catch(error => {
+					console.log('error', error.message);
+				});
+		}
+	});
+
+	return (
+		<KeyboardAvoidingView behavior='padding' style={styles.container}>
+			<StatusBar style='light' />
+			<Button
+				title='Login with Spotify'
+				onPress={() => {
+					promptAsync();
+				}}
+			/>
+			<View style={{ height: 100 }} />
+		</KeyboardAvoidingView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'black',
+	},
 });
