dff21eacfb77bf94b149e59efffaa357b698f8ff
Alik Ovesyan <icoleksander.Ovesian@americanwell.com>
2022-06-24 20:16:46 +0200

    refactoring

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import FirstComponent from './FirstRenderedComponent';
+import { FirstComponent } from './FirstRenderedComponent';
 import store from './store/store';
 // import store from './store/store';
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <FirstComponent />
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<FirstComponent />
+		</Provider>
+	);
 }
 
 {
-  /* {a === 'apikey123' ? (
+	/* {a === 'apikey123' ? (
   <SafeAreaProvider>
     <Navigation colorScheme={colorScheme} />
     <StatusBar />
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -4,90 +4,91 @@ import { View, StyleSheet, KeyboardAvoidingView, Button } from 'react-native';
 import { useEffect, useState } from 'react';
 import { ResponseType, useAuthRequest } from 'expo-auth-session';
 import axios from 'axios';
-import { API_CONFIG } from '../constants/api';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useDispatch } from 'react-redux';
+
+import { API_CONFIG } from '../constants/api';
 import { setAccessToken } from '../store/reducers/login.reducer';
 
-export default function LoginScreen() {
-  const dispatch = useDispatch();
-  const [token, setToken] = useState('');
-  const [request, response, promptAsync] = useAuthRequest(
-    {
-      responseType: ResponseType.Token,
-      clientId: API_CONFIG.clientId,
-      scopes: [
-        'user-read-currently-playing',
-        'user-read-recently-played',
-        'user-read-playback-state',
-        'user-top-read',
-        'user-modify-playback-state',
-        'streaming',
-        'user-read-email',
-        'user-read-private',
-      ],
-      usePKCE: false,
-      redirectUri: API_CONFIG.redirectUri,
-    },
-    {
-      authorizationEndpoint: 'https://accounts.spotify.com/authorize',
-      tokenEndpoint: 'https://accounts.spotify.com/api/token',
-    }
-  );
+export const LoginScreen = () => {
+	const dispatch = useDispatch();
+	const [token, setToken] = useState('');
+	const [request, response, promptAsync] = useAuthRequest(
+		{
+			responseType: ResponseType.Token,
+			clientId: API_CONFIG.clientId,
+			scopes: [
+				'user-read-currently-playing',
+				'user-read-recently-played',
+				'user-read-playback-state',
+				'user-top-read',
+				'user-modify-playback-state',
+				'streaming',
+				'user-read-email',
+				'user-read-private',
+			],
+			usePKCE: false,
+			redirectUri: API_CONFIG.redirectUri,
+		},
+		{
+			authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+			tokenEndpoint: 'https://accounts.spotify.com/api/token',
+		}
+	);
 
-  const storeData = async (value: string) => {
-    try {
-      await AsyncStorage.setItem('api_key', value);
-    } catch (e) {}
-  };
+	const storeData = async (value: string) => {
+		try {
+			await AsyncStorage.setItem('api_key', value);
+		} catch (e) {}
+	};
 
-  useEffect(() => {
-    if (response?.type === 'success') {
-      const { access_token } = response.params;
-      setToken(access_token);
-      storeData(access_token);
-      dispatch(setAccessToken(access_token));
-    }
-  }, [response]);
+	useEffect(() => {
+		if (response?.type === 'success') {
+			const { access_token } = response.params;
+			setToken(access_token);
+			storeData(access_token);
+			dispatch(setAccessToken(access_token));
+		}
+	}, [response]);
 
-  useEffect(() => {
-    if (token) {
-      axios('https://api.spotify.com/v1/me', {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + token,
-        },
-      })
-        .then((response) => {
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.log('error', error.message);
-        });
-    }
-  });
+	useEffect(() => {
+		if (token) {
+			axios('https://api.spotify.com/v1/me', {
+				method: 'GET',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+					Authorization: 'Bearer ' + token,
+				},
+			})
+				.then(response => {
+					console.log(response.data);
+				})
+				.catch(error => {
+					console.log('error', error.message);
+				});
+		}
+	});
 
-  return (
-    <KeyboardAvoidingView behavior='padding' style={styles.container}>
-      <StatusBar style='light' />
-      <Button
-        title={'Login with Spotify'}
-        onPress={() => {
-          promptAsync();
-        }}
-      />
-      <View style={{ height: 100 }} />
-    </KeyboardAvoidingView>
-  );
-}
+	return (
+		<KeyboardAvoidingView behavior='padding' style={styles.container}>
+			<StatusBar style='light' />
+			<Button
+				title={'Login with Spotify'}
+				onPress={() => {
+					promptAsync();
+				}}
+			/>
+			<View style={{ height: 100 }} />
+		</KeyboardAvoidingView>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'black',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'black',
+	},
 });
